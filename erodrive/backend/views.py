b3c42d9c4fc1d601031ee55ee6fdd0a68ba1516a
Rob Lee <robstraightedge4life@gmail.com>
2020-01-08 14:43:32 +0800

    installation step 2: split 2 part for local and online enviroment

--- erodrive/backend/views.py
+++ erodrive/backend/views.py
@@ -75,12 +75,16 @@ def install_1(request):
     helpers.batch_store_config(data)
     one = OneDrive()
     curr_url = None
-    if request.get_host() == 'localhost':
+    curr_host = request.get_host().split(':', -1)[0]
+    if curr_host == 'localhost':
         http_referrer = request.get_raw_uri().split(':', -1)[0] + '://'
         curr_url = http_referrer + request.get_host() + '/admin/install'
-
     oauth_url = one.get_authorize_url(curr_url)
-    return render(request, 'install/2.html', {'oauth_url': oauth_url})
+
+    if curr_host == 'localhost':
+        return render(request, 'install/2-local.html', {'oauth_url': oauth_url})
+    else:
+        return render(request, 'install/2.html', {'oauth_url': oauth_url})
 
 
 def install_2(request, code):
